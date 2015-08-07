ce5f000b7decf6c3e55e489becdb0403364528a4
St00pid <einsteingr@gmail.com>
2015-08-07 10:25:34 +0200

    contains all actions,items, phrases and player names
    This game draws theme from Anime world.
    mainlist.py contains all the lists where the game draws actions,items, phrases and player names from.
    
    mood list is the only one that shouldn't be altered.
    
    Adding players:
    
    "Player name" : "http://youravatarurlhere.com",
    "Another player" : "http://otheravatar.com"
    
    
    
    adding legendary items: "item 1", "item2"
    enter list: entrance description.
    fight : fight phrases
    nofight : nofight phrases
    powerup: final battle phrases
    ways to die: upon death a phrase will be taken from here

---
+++ mainlist.py
@@ -0,0 +1,63 @@
+legendary_items = [ "Death Note", "Future Diary", "The Treasured Tools of the Sage of the Six Paths", "Soshuga", "Yoru", "Poke Ball", "soul gem", "Wolfwood's Punisher","Sech's Titan Blade",
+"Zabuza's Kubikiri Hocho","Gyro's steel balls",  "Hisoka's cards", "Feitan's Umbrella",  "Berserker Armor", "Behelit", "senzu beans", "Joseph's clackers", "Zatch Bell's spellbooks",
+"Sasuke's Kusanagi", "Temari's Fan", "Scrolls", "Sakura's antidote"
+]
+
+ways_to_die = ["had A Heart Attack...", "ass got kamehame'd...", 
+"used the technique that returned Kurama to a younger age in order to beat him as an inexperienced child. Little did he know, Kurama was a fucking beast in his prime...",
+"'s head got smashed!", "got attacked by blackbeard crew and died... ", "gets captured by Akane. Akane slits her throat..."
+]
+
+powerup = ["screams in rage and ascends to SSJ2!!!", "rages and his arm is transformed into an Angel Arm!!!", "is consumed by rage and nen transforms him into an adult!!!", 
+"opens the 8th gate of death!!!", "goes into  Sage mode and summons the 1000 hands of buddah!!!", "eats 18 and becomes perfect!!!", "uses both hands to cut down his enemy!!!",
+"rages and gathers ki. Turns into SSJ2 and releases an instant ki flash!!!", "reflects and meditates. He can now cut nothing!!!", "goes into gear 4!!!", "uses fire breath and turns into dragon!!!"
+]
+
+
+mood = ["Fight", "Search", "Ponder"]
+
+ponder = ["is sitting under a tree playing an ocarina.", "is looking for the dragonballs.", "travels to the universe.", "is eating ramen.", 
+"is crying alone in a corner", "wants to know the meaning of life.", "is listening to the birds sing.", "takes a nap."]
+
+nofight = ["decided to go for fishing...", "was looking for a fight but nobody gave a shit...", "got scared and tried to hide under a rock...",
+"fucked off to never land for a bit. It's safer there..."]
+
+fight = ["Ki blasts were flying all over the place. Everyone was kungfu fighting...", 
+"They raised their fists up. Without warning, screamed a war cry and attacked! You could feel the ground shake, the earth drank much blood that day!",
+"A fierce battle of rock, paper and scissors has begun!", "A fierce head-butt struggle begins!" ]
+
+enter = ["looking hot as hell", "is ready to kick ass!", "is feeling paranoid", "has a significant understanding of what is going on and wants to set things right.",
+"will take you to places you never expected not to visit!", "looks like a cranky old lady", "will use trickery to win this game", "looks like a sound you heard that bathes in sunlight.",
+"is omni-present, much like candy.", "is not yet ready to die.", "'s style does not make any sense", "farts in a sound that could please even the most demanding follower of Freud.",
+"slips on a banana peel.", "'s skin is crawling...", "gambles with lives, happiness, and even destiny itself!", "is like a fashion icon lay down on the riverbed.", "'s pants are already full of pee...",
+"'s getting all fired up!", "enter's the game but  doesn't look very coherent."
+]
+
+playerdir = { 
+"Player1" : "http://i.imgur.com/68jKje0.gif",
+"Player2" : "http://i.imgur.com/k4j8Gbz.gif",
+"Player3" : "http://i.imgur.com/hJg7nvi.gif",
+"Player4" : "http://i.imgur.com/XDZ34Db.gif",
+"Player5" : "http://i.imgur.com/cRR2vx3.gif",
+"Player6" : "http://i.imgur.com/ClceaKX.gif",
+"Player7" : "http://i.imgur.com/YMdNzST.gif",
+"Player8" : "http://i.imgur.com/tHL4yRh.gif",
+"Player9" : "http://i.imgur.com/wfpYtyc.gif",
+"Player10" : "http://i.imgur.com/t3OVMJp.gif",
+"Player11" : "http://i.imgur.com/NcrcHfO.gif",
+"Player12" : "http://i.imgur.com/2xvdJz2.gif",
+"Player13" : "http://i.imgur.com/Q9sWTcr.gif",
+"Player14" : "http://i.imgur.com/mPW9QVZ.gif",
+"Player15" : "http://i.imgur.com/378pKpU.gif",
+"Player16" : "http://i.imgur.com/auXbLAJ.gif",
+"Player17" : "http://i.imgur.com/RyPml56.gif",
+"Player18" : "http://i.imgur.com/5CkiZYL.gif",
+"Player19" : "http://i.imgur.com/HT7ncZW.gif",
+"Player20" : "http://i.imgur.com/waGYHVI.gif",
+"Player21" : "http://i.imgur.com/IGN8dLV.gif",
+"Player22" : "http://i.imgur.com/v2BfeI2.gif",
+"Player23" : "http://i.imgur.com/Kf4ddCs.gif",
+"Player24" : "http://i.imgur.com/ujRgko2.gif",
+"Player25" : "http://i.imgur.com/KIETq3b.gif",
+"Player26" : "http://i.imgur.com/Nw91HIt.gif"
+}
